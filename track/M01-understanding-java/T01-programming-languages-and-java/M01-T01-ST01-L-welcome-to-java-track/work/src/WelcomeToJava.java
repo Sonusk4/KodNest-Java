8af56cddfe630466c12709e82ee47cd06889f7de
Sonusk4 <sonuskyadav20@gmail.com>
2026-07-30 12:46:55 +0530

    day 1 changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("welcome to java track");
+        System.out.println("NOW I KNOW HOW TO BUILD THE LOGIC");
+        System.out.println("now i learning how to express that logic into java");
+        // Write your code here
     }
 }
