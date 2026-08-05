a213d2b921688f5785cce5ca44e8741078cd7e90
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-05 12:21:08 +0530

    for loop pratice

---
+++ track/M02/pfor.java
@@ -0,0 +1,9 @@
+package track.M02;
+
+public class pfor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
--- track/M02/stat_switch.java
+++ track/M02/stat_switch.java
@@ -1,34 +1,23 @@
-package track.M02;
-
 public class stat_switch {
     public static void main(String[] args) {
-
-        int day = 3;
+        int day = 6;
 
         switch (day) {
             case 1:
-                System.out.println("Monday");
-                break;
             case 2:
-                System.out.println("Tuesday");
-                break;
             case 3:
-                System.out.println("Wednesday");
-                break;
             case 4:
-                System.out.println("Thursday");
-                break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("Learning Day");
                 break;
             case 6:
-                System.out.println("Saturday");
-                break;
             case 7:
-                System.out.println("Sunday");
+                System.out.println("Revision Day");
                 break;
             default:
-                System.out.println("Invalid day");
+                System.out.println("error");
         }
+
+        // Write the switch statement
     }
 }
