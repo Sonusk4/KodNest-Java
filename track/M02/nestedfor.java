99110b5f8d9114ac7a06c35facb34a3881055c54
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-06 11:37:57 +0530

    nestedfor

---
+++ track/M02/nestedfor.java
@@ -0,0 +1,12 @@
+package track.M02;
+
+public class nestedfor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+    }
+}
