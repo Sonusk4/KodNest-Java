569cb894697ab37f6f05cda4608a24010f2b225d
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-06 12:23:51 +0530

    break and continue

---
+++ track/M02/breakp.java
@@ -0,0 +1,11 @@
+package track.M02;
+
+public class breakp {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5)
+                break;
+            System.out.println(i);
+        }
+    }
+}
