f66ca455c0201ccaa6af90ac510169e40a1e4a6f
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-06 12:42:57 +0530

    nested for with break

---
+++ track/M02/nestedforwithbreak.java
@@ -0,0 +1,14 @@
+package track.M02;
+
+public class nestedforwithbreak {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == 3)
+                    break;
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+    }
+}
