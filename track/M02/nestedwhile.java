4c785fde68caeb9be12e3d68409ab7abc87b43a0
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-06 11:39:02 +0530

    nested while

---
+++ track/M02/nestedwhile.java
@@ -0,0 +1,16 @@
+package track.M02;
+
+public class nestedwhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 4) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
