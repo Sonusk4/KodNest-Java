ca0d6ea1ce12458447b081e15a09b7c384472bbc
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-10 13:05:29 +0530

    object and class

---
+++ track/M02/StudentApplication.java
@@ -0,0 +1,24 @@
+package track.M02;
+
+class Student {
+    int age;
+    String name;
+    int height;
+
+    void sleep() {
+        System.out.println("sleeping");
+    }
+}
+
+public class StudentApplication {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.age = 10;
+        s1.name = "sonu";
+        s1.height = 20;
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.height);
+        s1.sleep();
+    }
+}
