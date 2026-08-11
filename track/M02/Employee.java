5910d30033a14cfa7a2b288c690aa0b5c5eb6c8a
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-11 11:12:51 +0530

    classes

---
+++ track/M02/Employee.java
@@ -0,0 +1,24 @@
+package track.M02;
+
+class Student {
+    int id;
+    String name;
+}
+
+class Employee {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 20;
+        s1.name = "sonu";
+
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+
+        Student s2;
+        s2 = s1;
+        s2.id = 10;
+        s2.name = "ankit";
+        System.out.println(s2.id);
+        System.out.println(s2.name);
+    }
+}
