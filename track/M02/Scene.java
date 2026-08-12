690a5aac394723dbd51e109201e0ad1b87720536
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-12 16:19:08 +0530

    oops

---
+++ track/M02/Scene.java
@@ -0,0 +1,26 @@
+package track.M02;
+
+class Mobile {
+    int pass = 123;
+
+    void phone(String name) {
+        System.out.println(name);
+    }
+
+    void phone(int pass) {
+        if (this.pass == pass) {
+            System.out.println("call connected");
+        } else {
+            System.out.println("call not connected");
+        }
+    }
+
+}
+
+public class Scene {
+    public static void main(String[] args) {
+        Mobile m = new Mobile();
+        m.phone("sonu");
+        m.phone(123);
+    }
+}
