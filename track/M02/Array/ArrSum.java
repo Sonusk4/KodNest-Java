d72a18ae9d73276bed7a3f7f3babb1f9039b4a95
Sonusk4 <sonuskyadav20@gmail.com>
2026-08-18 10:57:44 +0530

    array sum

---
+++ track/M02/Array/ArrSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("enter the elements");
+        for (int i = 0; i < 5; i++) {
+
+            a[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum = sum + a[i];
+        }
+        System.out.println(sum);
+    }
+}
